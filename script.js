74d2ff49523ae7075d6901e14809d67707d3d74b
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-10-04 17:36:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 function highlight() {
     //Write your code here
+    let strong = document.querySelectorAll("strong");
+    // console.log(strong);
+    
 
-
+    strong.forEach((boldie => {
+        
+        boldie.style.color = "green"
+    }))
+    
+    
+    
 }
 
 
 function return_normal() {
     //Write your code here
+    let strong = document.querySelectorAll("strong");
+    // console.log(strong);
+    
+    
+    strong.forEach((boldie => {
+        
+        boldie.style.color = "black"
+    }))
 
     
 }
